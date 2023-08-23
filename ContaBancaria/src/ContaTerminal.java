6169b447ec72f8aeb9cf882e44e8ab84f91254a0
Leonardo Ruiz Bottura <92493176+LeonardoRuizB@users.noreply.github.com>
2023-08-23 16:30:38 -0300

    feat: Finished project

--- ContaBancaria/src/ContaTerminal.java
+++ ContaBancaria/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite seu nome!");
+        String nome = sc.nextLine();
+
+        System.out.println("Por favor, digite o saldo!");
+        Double saldo = sc.nextDouble();
+
+        sc.close();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
